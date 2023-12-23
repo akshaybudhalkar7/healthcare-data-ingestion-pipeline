9d5ef421bb3b26463b35a230ea5a9fe789afb051
unknown <akshaybudhalkar7@gmail.com>
2023-12-23 20:47:27 +0530

    added subnet type

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -20,7 +20,7 @@ class S3Stack(Stack):
                           ),
                           ec2.SubnetConfiguration(
                               name="Private",
-                              subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                              subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT,
                               cidr_mask=20
                           )
                           ],
