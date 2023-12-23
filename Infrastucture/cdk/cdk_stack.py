d9aecdff1cd14dd00447b1e6fe5d1a4c5a4553a1
unknown <akshaybudhalkar7@gmail.com>
2023-12-23 22:03:19 +0530

    changed menthod

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -25,7 +25,7 @@ class S3Stack(Stack):
         instance_type = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO)
         engine_version = aws_rds.MysqlEngineVersion.VER_8_0_28
 
-        subnets = vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED)
+        subnets = vpc.selectSubnets(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED)
 
         # Create RDS Instance
         rds = aws_rds.DatabaseInstance(self, "heathcaredb",
