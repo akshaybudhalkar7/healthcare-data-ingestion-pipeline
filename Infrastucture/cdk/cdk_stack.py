cde06f29b4efacff55569ca35f84236de1e39d88
unknown <akshaybudhalkar7@gmail.com>
2023-12-23 20:42:43 +0530

    added vpc

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -31,6 +31,6 @@ class S3Stack(Stack):
                                        instance_type=instance_type,
                                        # master_username="admin",
                                        # master_user_password="akshay@123",
-                                       # vpc=vpc  # Specify your VPC
+                                       vpc=vpc  # Specify your VPC
                                        # vpc_placement={"subnet_type": ec2.SubnetType.PUBLIC}
                                        )
