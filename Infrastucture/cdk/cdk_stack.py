539b16a5ce8bdb54c48590acb9456bb86873d7dd
unknown <akshaybudhalkar7@gmail.com>
2023-12-23 20:45:11 +0530

    added subnet type

--- Infrastucture/cdk/cdk_stack.py
+++ Infrastucture/cdk/cdk_stack.py
@@ -17,10 +17,14 @@ class S3Stack(Stack):
                               name="Private",
                               subnet_type=ec2.SubnetType.PRIVATE_ISOLATED,
                               cidr_mask=20
+                          ),
+                          ec2.SubnetConfiguration(
+                              name="Private",
+                              subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                              cidr_mask=20
                           )
                           ],
                       nat_gateways=1)
-
 
         # Free tier-eligible instance type
         instance_type = ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO)
